d4961280f15d94fdeff4844320e9dc392e525448
Sunil Rasaily <sunilrasaily02@gmail.com>
2022-09-20 16:55:45 -0500

    Update FindTreasureApp.Designer.cs

--- FindTreasureApp/FindTreasureApp.Designer.cs
+++ FindTreasureApp/FindTreasureApp.Designer.cs
@@ -44,8 +44,10 @@ namespace FindTreasureApp
             this.gameHeader.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.gameHeader.Controls.Add(this.scoreLabel);
             this.gameHeader.Location = new System.Drawing.Point(0, 0);
+            this.gameHeader.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gameHeader.Name = "gameHeader";
-            this.gameHeader.Size = new System.Drawing.Size(800, 50);
+            this.gameHeader.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gameHeader.Size = new System.Drawing.Size(1067, 62);
             this.gameHeader.TabIndex = 0;
             this.gameHeader.TabStop = false;
             // 
@@ -53,9 +55,10 @@ namespace FindTreasureApp
             // 
             this.scoreLabel.AutoSize = true;
             this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreLabel.Location = new System.Drawing.Point(322, 9);
+            this.scoreLabel.Location = new System.Drawing.Point(429, 11);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(144, 37);
+            this.scoreLabel.Size = new System.Drawing.Size(178, 46);
             this.scoreLabel.TabIndex = 0;
             this.scoreLabel.Text = "Score: 0";
             this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -63,19 +66,22 @@ namespace FindTreasureApp
             // gameFooter
             // 
             this.gameFooter.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.gameFooter.Location = new System.Drawing.Point(0, 400);
+            this.gameFooter.Location = new System.Drawing.Point(0, 492);
+            this.gameFooter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gameFooter.Name = "gameFooter";
-            this.gameFooter.Size = new System.Drawing.Size(800, 50);
+            this.gameFooter.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gameFooter.Size = new System.Drawing.Size(1067, 62);
             this.gameFooter.TabIndex = 1;
             this.gameFooter.TabStop = false;
             // 
             // FindTreasureApp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.gameFooter);
             this.Controls.Add(this.gameHeader);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FindTreasureApp";
             this.Text = "Find the Treasure";
             this.Shown += new System.EventHandler(this.OnAppShown);
